08b251dfb60c0f49a380d284377550c494864712
George Wyner <gwyner@computer.org>
2018-07-17 12:14:45 -0400

    updated google tests and worked on writeTab

--- lambda/us-east-1_ClassroomAssistant/test-google-sdk.js
+++ lambda/us-east-1_ClassroomAssistant/test-google-sdk.js
@@ -5,8 +5,7 @@ const googleSDK = require('./GoogleSDK.js');
 console.log('googleSDK = ' + Object.keys(googleSDK));
 
 // read in some sheets to test readTab
-
-
+// to see actual values, currently debugger is best
 async function testRead() {
 
     let scheduleObj = await googleSDK.readTab("1f_zgHHi8ZbS6j0WsIQpbkcpvhNamT2V48GuLc0odyJ0", "Schedule");
@@ -21,3 +20,34 @@ async function testRead() {
 }
 
 testRead();
+
+
+// simple test of write
+
+// read sheet and then update?
+// how do read and write play?
+
+
+
+
+async function testWrite() {
+
+    const sheetData = {
+        // omit? "range": "A1:D5",
+        "majorDimension": "ROWS",
+        "values": [
+            ["Item", "Cost", "Stocked", "Ship Date"],
+            ["Wheel", "$20.50", "4", "3/1/2016"],
+            ["Door", "$15", "2", "3/15/2016"],
+            ["Engine", "$100", "1", "30/20/2016"],
+            ["Totals", "=SUM(B2:B4)", "=SUM(C2:C4)", "=MAX(D2:D4)"]
+        ]
+    };
+
+    const keyList = "Engine";
+    const valueList = {"header":"Cost", "value": "1,000.99"};
+
+    googleSDK.writeTab("1f_zgHHi8ZbS6j0WsIQpbkcpvhNamT2V48GuLc0odyJ0", "WriteTest", keyList, valueList);
+}
+
+testWrite();
