afd51c2df1f25f7e35638cc2a7d211d6024517c7
Pete Sterpe <sterpe@bc.edu>
2018-07-17 16:04:09 -0400

    Revised writeTab()

--- lambda/us-east-1_ClassroomAssistant/test-google-sdk.js
+++ lambda/us-east-1_ClassroomAssistant/test-google-sdk.js
@@ -32,22 +32,15 @@ testRead();
 
 async function testWrite() {
 
-    const sheetData = {
-        // omit? "range": "A1:D5",
-        "majorDimension": "ROWS",
-        "values": [
-            ["Item", "Cost", "Stocked", "Ship Date"],
-            ["Wheel", "$20.50", "4", "3/1/2016"],
-            ["Door", "$15", "2", "3/15/2016"],
-            ["Engine", "$100", "1", "30/20/2016"],
-            ["Totals", "=SUM(B2:B4)", "=SUM(C2:C4)", "=MAX(D2:D4)"]
-        ]
+    const keys = {
+        Category: 'drivetrain',
+        SubCategory: 'transmission',
+        Item: 'engine'
     };
 
-    const keyList = "Engine";
-    const valueList = {"header":"Cost", "value": "1,000.99"};
+    const values = {"Ship Date":"7/18/2018"};
 
-    googleSDK.writeTab("1f_zgHHi8ZbS6j0WsIQpbkcpvhNamT2V48GuLc0odyJ0", "WriteTest", keyList, valueList);
+    googleSDK.writeTab("1f_zgHHi8ZbS6j0WsIQpbkcpvhNamT2V48GuLc0odyJ0", "WriteTest", keys, values);
 }
 
 testWrite();
