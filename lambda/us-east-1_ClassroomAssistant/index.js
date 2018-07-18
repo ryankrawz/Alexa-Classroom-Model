41fb70947aeffec87f4f5f57313b382df77b6c90
Daewoo Joeng <jeongdb@bc.edu>
2018-07-18 15:32:35 -0400

    fix on writetab for cold call

--- lambda/us-east-1_ClassroomAssistant/index.js
+++ lambda/us-east-1_ClassroomAssistant/index.js
@@ -740,7 +740,7 @@ const handlers = {
                     NickName: speechOutput
                  };
                 let values = {
-                    BeenCalled: this.attributes.rosterObj[this.attributes.courseNumber][this.attributes.sectionNumber][speechOutput]["BeenCalled"]
+                    BeenCalled: (this.attributes.rosterObj[this.attributes.courseNumber][this.attributes.sectionNumber][speechOutput]["BeenCalled"] + 1)
                 };
                 googleSDK.writeTab(spreadsheetID, "Roster", keys, values);
 
